c3194b1eaf5e5893c0ff6b588b331156e57c9b25
rahulmohonto <rahulmohonto1999@gmail.com>
2021-06-27 03:25:34 +0600

    sessionstorage login fix

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -23,7 +23,7 @@ const PrivateRoute = ({ children, ...rest }) => {
         <Route
             {...rest}
             render={({ location }) =>
-                (loggedInUser.email) ? (
+                (loggedInUser.email || sessionStorage.getItem('token')) ? (
                     children
                 ) : (
                     <Redirect
